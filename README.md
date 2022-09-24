08119db08bfad2bcaa3eed33b81b6d6bacca6bf7
Luis Fernando M. Petreca <lfpetreca02@gmail.com>
2022-09-24 14:41:48 -0300

    Camada Application

---
+++ README.md
@@ -0,0 +1,16 @@
+### Useful commands
+
+#### RUN Database
+```bash
+docker run --name mysql-dev -p 3606:3606 -e MYSQL_ROOT_PASSWORD=PA$$ -d mysql:8.0
+```
+
+### DataGrip
+```bash
+CREATE DATABASE  if not exists abc_tech;
+```
+
+### Start SpringBoot
+```bash
+Run `AbctechapiApplication` Shift + F10
+```
---
+++ src/main/java/br/com/fiap/abctechapi/application/impl/AssistanceApplicationImpl.java
@@ -0,0 +1,14 @@
+package br.com.fiap.abctechapi.application.impl;
+
+import br.com.fiap.abctechapi.application.AssistanceApplication;
+import br.com.fiap.abctechapi.application.dto.AssistDto;
+
+import java.util.List;
+
+public class AssistanceApplicationImpl implements AssistanceApplication {
+
+    @Override
+    public List<AssistDto> getAssists() {
+        return null;
+    }
+}
